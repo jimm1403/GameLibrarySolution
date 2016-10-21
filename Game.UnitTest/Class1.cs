1f0608ae56cd30a3b080dad5d0ec564d50c846d0
jimmi <jimmi@DESKTOP-2OTI0T5>
2016-10-21 13:14:08 +0200

    Add project files.

---
+++ Game.UnitTest/Class1.cs
@@ -0,0 +1,33 @@
+﻿using GameLibrary;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.UnitTest
+{
+    [TestFixture]
+    public class GameTest   
+    {
+        Ship ship;
+
+        [SetUp]
+        public void SetUpUnitTest()
+        {
+            ship = new Ship();
+        }
+
+        [Test]
+        public void CanShipTakeDamage()
+        {
+            int expected = 4;
+
+            int actual = ship.TakeDamage();
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
